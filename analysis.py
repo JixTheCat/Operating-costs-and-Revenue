cb390cbeb66fbf9bc447cd990c0367cce9d04c38
JixTheCat <bryce.polley@gmail.com>
2023-08-22 16:45:04 +0930

    fixed importance and categoric variables

--- analysis.py
+++ analysis.py
@@ -11,32 +11,64 @@ from sklearn.metrics import accuracy_score
 
 import random
 
-random.seed(0)
+random.seed(100)
 
 ################################################################################
 # Setup
 df = pd.read_csv("dfb.csv")
 
-df["nh_disease"] = bin_col(df["nh_disease"].replace({np.nan: 0})**2)
-
-cols = ['tonnes_grapes_harvested'
-    , 'area_harvested'
-    , 'water_used'
-    , 'total_tractor_passes'
+cols = ["tonnes_grapes_harvested"
+    , "area_harvested"
+    , "water_used"
+    , "total_tractor_passes"
     , "total_fertiliser"
-    #, 'total_vineyard_fuel'
-    #, 'total_vineyard_electricity'
-    #, 'total_irrigation_area'
-    , 'synthetic_nitrogen_applied'
-    , 'organic_nitrogen_applied'
-    , 'synthetic_fertiliser_applied'
-    , 'organic_fertiliser_applied'
-    #, 'area_not_harvested'
-    #, 'total_irrigation_electricity'
-    #, 'total_irrigation_fuel'
-    , 'giregion'
-    #, 'vineyard_area_white_grapes'
-    #, 'vineyard_area_red_grapes'
+    , "synthetic_nitrogen_applied"
+    , "organic_nitrogen_applied"
+    , "synthetic_fertiliser_applied"
+    , "organic_fertiliser_applied"
+    , "giregion"
+    , "data_year_id"
+    , "river_water"
+    , "groundwater"
+    , "surface_water_dam"
+    , "recycled_water_from_other_source"
+    , "mains_water"
+    , "other_water"
+    , "water_applied_for_frost_control"
+    , "bare_soil"
+    , "annual_cover_crop"
+    , "permanent_cover_crop_native"
+    , "permanent_cover_crop_non_native"
+    , "permanent_cover_crop_volunteer_sward"
+    , "diesel_vineyard"
+    , "electricity_vineyard"
+    , "petrol_vineyard"
+    , "vineyard_solar"
+    , "vineyard_wind"
+    , "lpg_vineyard"
+    , "biodiesel_vineyard"
+    , "slashing_number_of_times_passes_per_year"
+    , "fungicide_spraying_number_of_times_passes_per_year"
+    , "herbicide_spraying_number_of_times_passes_per_year"
+    , "insecticide_spraying_number_of_times_passes_per_year"
+]
+
+
+cat_cols = [ # These are binary
+    "bare_soil"
+    , "annual_cover_crop"
+    , "permanent_cover_crop_native"
+    , "permanent_cover_crop_non_native"
+    , "permanent_cover_crop_volunteer_sward"
+    , "irrigation_energy_diesel"
+    , "irrigation_energy_electricity"
+    , "irrigation_energy_pressure"
+    , "irrigation_energy_solar"
+    , "irrigation_type_dripper"
+    , "irrigation_type_flood"
+    , "irrigation_type_non_irrigated"
+    , "irrigation_type_overhead_sprinkler"
+    , "irrigation_type_undervine_sprinkler"
     , 'river_water'
     , 'groundwater'
     , 'surface_water_dam'
@@ -44,90 +76,82 @@ cols = ['tonnes_grapes_harvested'
     , 'mains_water'
     , 'other_water'
     , 'water_applied_for_frost_control'
-    , 'bare_soil'
-    , 'annual_cover_crop'
-    , 'permanent_cover_crop_native'
-    , 'permanent_cover_crop_non_native'
-    , 'permanent_cover_crop_volunteer_sward'
-    #, 'irrigation_energy_diesel'
-    #, 'irrigation_energy_electricity'
-    #, 'irrigation_energy_pressure'
-    #, 'irrigation_energy_solar'
-    #, 'irrigation_type_dripper'
-    #, 'irrigation_type_flood'
-    #, 'irrigation_type_non_irrigated'
-    #, 'irrigation_type_overhead_sprinkler'
-    #, 'irrigation_type_undervine_sprinkler'
-    , 'diesel_vineyard'
-    , 'electricity_vineyard'
-    , 'petrol_vineyard'
-    , 'vineyard_solar'
-    , 'vineyard_wind'
-    , 'lpg_vineyard'
-    , 'biodiesel_vineyard'
-    , 'slashing_number_of_times_passes_per_year'
-    , 'fungicide_spraying_number_of_times_passes_per_year'
-    , 'herbicide_spraying_number_of_times_passes_per_year'
-    , 'insecticide_spraying_number_of_times_passes_per_year'
-    #, 'nh_disease'
-    #, 'nh_frost'
-    #, 'nh_new_development'
-    #, 'nh_non_sale'
-    #, 'off_farm_income'
-    #, 'prev_avg'
+    , "nh_frost"
+    , "nh_disease"
+    , "data_year_id" # These are one hot encoded
+    , "giregion"
 ]
 
-# X, y setup
-# Currently giregion is in both the predicted and predictors! You gotta fix that
-X = df[cols].join(pd.get_dummies(df["giregion"])).drop(["giregion"], axis=1)
-# Make sure that the y variable is not in the X variable or you will have a lot of trouble!!
 
-# we add a random number for baselining importance
+for column in cat_cols:
+    df[column] = pd.Categorical(df[column])
 
-#X["random"] = np.random.normal(0, 1, len(X))
-i = 0
-giregion_lookup = {}
-for giregion in df["giregion"].unique():
-    giregion_lookup[giregion] = i
-    i += 1
-df["giregion"] = df["giregion"].replace(giregion_lookup)
+y_name = "bare_soil"
+categoric = True
 
-# split into train test val
-y = df["nh_disease"] 
-X_train, X_test, y_train, y_test \
-    = train_test_split(X, y, test_size=0.1, random_state=1)
+model = train_model(df[cols], y_name, categoric)
 
-X_train, X_val, y_train, y_val \
-    = train_test_split(X_train, y_train, test_size=0.1/0.9, random_state=1) 
+def train_model(df: pd.DataFrame, y_name: str, categoric: bool, test_size=0.1, val_size=0):
+    """Trains an XGBoosted Tree given a y variable and dataframe. """
+    # X, y setup
+    # Currently giregion is in both the predicted and predictors! You gotta fix that
+    X = df.drop([y_name], axis=1)
+    X = pd.get_dummies(X)
+    # Make sure that the y variable is not in the X variable or you will have a lot of trouble!!
 
-dtrain = xgb.DMatrix(X_train, label=y_train.apply(int), enable_categorical=True)
-dtest = xgb.DMatrix(X_test, label=y_test, enable_categorical=True)
-dval = xgb.DMatrix(X_val, label=y_val, enable_categorical=True)
+    # we add a random number for baselining importance
 
-# setup model
+    #X["random"] = np.random.normal(0, 1, len(X))
+    # i = 0
+    # giregion_lookup = {}
+    # for giregion in df["giregion"].unique():
+    #     giregion_lookup[giregion] = i
+    #     i += 1
+    # df["giregion"] = df["giregion"].replace(giregion_lookup)
 
-param = {"colsample_bytree": 0.3, "learning_rate": 0.00025, 'objective': 'binary:logistic'}
+    # split into train test val
+    y = df[y_name]
+    X_train, X_test, y_train, y_test \
+        = train_test_split(X, y, test_size=test_size, random_state=1)
 
-param['eval_metric'] = 'auc'
+    if val_size != 0:
+        X_train, X_val, y_train, y_val \
+            = train_test_split(X_train, y_train, test_size=val_size, random_state=1) 
+    else:
+        y_val = 0
+        X_val = 0
 
-evallist = [(dtrain, 'train'), (dtest, 'eval')]
+    dtrain = xgb.DMatrix(X_train, label=y_train.apply(int), enable_categorical=categoric)
+    dtest = xgb.DMatrix(X_test, label=y_test, enable_categorical=categoric)
+    if val_size != 0:
+        dval = xgb.DMatrix(X_val, label=y_val, enable_categorical=categoric)
 
-num_round = 100
+    # setup model
 
+    param = {"colsample_bytree": 0.3, "learning_rate": 0.00025, "objective": "binary:logistic"}
 
-model = xgb.XGBClassifier(
-    learning_rate=0.005
-    , n_estimators=400
-    , scale_pos_weight=45)
+    param["eval_metric"] = "auc"
 
-ok = model.fit(X_train
-    , y_train
-    #, params = param
-    #, num_boost_round = num_round
-    #, nfold = 5
-    , eval_set=[(X_test, y_test)]
-    , verbose = True
-)
+    evallist = [(dtrain, "train"), (dtest, "eval")]
+
+    num_round = 100
+
+    model = xgb.XGBClassifier(
+        learning_rate=0.005
+        , n_estimators=400
+        , scale_pos_weight=45
+        , importance_type="gain")
+
+    model.fit(X_train
+        , y_train
+        #, params = param
+        #, num_boost_round = num_round
+        #, nfold = 5
+        , eval_set=[(X_test, y_test)]
+        , verbose = True
+    )
+
+    return model, X_val, y_val
 
 # kfold results
 
@@ -170,27 +194,26 @@ print(conf.groupby(["actual", "predicted"]).size().unstack(fill_value=0))
 xgb.plot_importance(model)
 plt.show()
 
-# importances = model.best_estimator_.feature_importances_
+# Feature importance!!!
+# thIS PERMUTATION IS ONLY FOR MODELS YOU WANT TO USE SUCH AS PROFIT AND OPERATIONAL COSTS.
+#
+#
+# importances = model.feature_importances_
+# feature_importance = model.feature_importances_
 
-feature_importance = model.best_estimator_.feature_importances_
-
-
-importances = model.feature_importances_
-feature_importance = model.feature_importances_
-
-sorted_idx = np.argsort(feature_importance)
-pos = np.arange(sorted_idx.shape[0]) + 0.5
-fig = plt.figure()
-result = permutation_importance(model, X, y, n_repeats=10)
-sorted_idx = result.importances_mean.argsort()[-30:]
-plt.boxplot(
-    result.importances[sorted_idx].T,
-    vert=False,
-    labels=np.array(model.feature_names_in_)[sorted_idx],
-)
-plt.title("Permutation Importance")
-fig.tight_layout()
-plt.show()
+# sorted_idx = np.argsort(feature_importance)
+# pos = np.arange(sorted_idx.shape[0]) + 0.5
+# fig = plt.figure()
+# result = permutation_importance(model, X, y, n_repeats=10)
+# sorted_idx = result.importances_mean.argsort()[-30:]
+# plt.boxplot(
+#     result.importances[sorted_idx].T,
+#     vert=False,
+#     labels=np.array(model.feature_names_in_)[sorted_idx],
+# )
+# plt.title("Permutation Importance")
+# fig.tight_layout()
+# plt.show()
 
 ####################################
 # Fixing for different classification levels
@@ -208,15 +231,15 @@ param_grid = dict(scale_pos_weight=weights)
 # define evaluation procedure
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 # define grid search
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=cv, scoring='roc_auc')
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=cv, scoring="roc_auc")
 # execute the grid search
 grid_result = grid.fit(X, y)
 # report the best configuration
 print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 # report all configurations
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
+means = grid_result.cv_results_["mean_test_score"]
+stds = grid_result.cv_results_["std_test_score"]
+params = grid_result.cv_results_["params"]
 for mean, stdev, param in zip(means, stds, params):
     print("%f (%f) with: %r" % (mean, stdev, param))
     
